5681fdabf5cd3c695a1275c3701c7efdf9574836
qutubuddin45 <qutubuddin011@gmail.com>
2025-04-24 19:23:22 +0530

    Update Blockchain-based Ticketing System.sol

--- contract/Blockchain-based Ticketing System.sol
+++ contract/Blockchain-based Ticketing System.sol
@@ -9,6 +9,11 @@ contract TicketingSystem {
 
     mapping(address => uint256) public ticketsOwned;
 
+    event TicketPurchased(address indexed buyer, uint256 quantity);
+    event TicketTransferred(address indexed from, address indexed to, uint256 quantity);
+    event TicketRefunded(address indexed holder, uint256 quantity);
+    event FundsWithdrawn(address indexed owner, uint256 amount);
+
     constructor(uint256 _ticketPrice, uint256 _totalTickets) {
         owner = msg.sender;
         ticketPrice = _ticketPrice;
@@ -27,9 +32,40 @@ contract TicketingSystem {
 
         ticketsOwned[msg.sender] += _quantity;
         ticketsSold += _quantity;
+
+        emit TicketPurchased(msg.sender, _quantity);
+    }
+
+    function getAvailableTickets() external view returns (uint256) {
+        return totalTickets - ticketsSold;
+    }
+
+    function transferTicket(address _to, uint256 _quantity) external {
+        require(_quantity > 0, "Quantity must be greater than 0");
+        require(ticketsOwned[msg.sender] >= _quantity, "Not enough tickets to transfer");
+
+        ticketsOwned[msg.sender] -= _quantity;
+        ticketsOwned[_to] += _quantity;
+
+        emit TicketTransferred(msg.sender, _to, _quantity);
+    }
+
+    function refundTicket(uint256 _quantity) external {
+        require(_quantity > 0, "Quantity must be greater than 0");
+        require(ticketsOwned[msg.sender] >= _quantity, "Not enough tickets to refund");
+
+        ticketsOwned[msg.sender] -= _quantity;
+        ticketsSold -= _quantity;
+
+        uint256 refundAmount = ticketPrice * _quantity;
+        payable(msg.sender).transfer(refundAmount);
+
+        emit TicketRefunded(msg.sender, _quantity);
     }
 
     function withdrawFunds() external onlyOwner {
-        payable(owner).transfer(address(this).balance);
+        uint256 amount = address(this).balance;
+        payable(owner).transfer(amount);
+        emit FundsWithdrawn(owner, amount);
     }
 }
